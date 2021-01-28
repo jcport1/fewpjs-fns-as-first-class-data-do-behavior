61c1bb4fbf457bf8b6fa0f622612bcf90d237e70
jeffersoncuartas <jefferson.cuartas@gmail.com>
2021-01-28 18:42:41 -0500

    Testing

--- index.js
+++ index.js
@@ -10,4 +10,19 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(timeString) {
+
+  //parseInt(string, radix) -> the radix is the numeral system
+  const hour = parseInt(timeString, 10);
+  if ( hour < 12) return "Good Morning"
+  if (hour > 17) return "Good Evening"
+  return "Good Afternoon"
+} 
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(mes){
+  document.getElementById("greeting").innerText = mes;
+}
+
